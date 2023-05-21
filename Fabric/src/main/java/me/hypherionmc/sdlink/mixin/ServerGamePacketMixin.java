ee1acdd1a27ed1118173758b41c8537f7f3ef51d
HypherionMC <hypherionmc@gmail.com>
2023-05-21 19:40:32 +0200

    [BUG] Fix Mixin Compatibility issue with Styled Chat

--- Fabric/src/main/java/me/hypherionmc/sdlink/mixin/ServerGamePacketMixin.java
+++ Fabric/src/main/java/me/hypherionmc/sdlink/mixin/ServerGamePacketMixin.java
@@ -3,18 +3,16 @@ package me.hypherionmc.sdlink.mixin;
 import me.hypherionmc.sdlink.SafeCalls;
 import me.hypherionmc.sdlink.server.ServerEvents;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
-import net.minecraft.server.players.PlayerList;
+import net.minecraft.server.network.TextFilter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.UUID;
-import java.util.function.Function;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * @author HypherionSA
@@ -25,25 +23,20 @@ public class ServerGamePacketMixin {
 
     @Shadow public ServerPlayer player;
 
-    @Redirect(
-            method = "handleChat(Lnet/minecraft/server/network/TextFilter$FilteredText;)V",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/server/players/PlayerList;broadcastMessage(Lnet/minecraft/network/chat/Component;Ljava/util/function/Function;Lnet/minecraft/network/chat/ChatType;Ljava/util/UUID;)V"
-            )
-    )
-    public void onGameMessage(PlayerList instance, Component filteredText, Function<ServerPlayer, Component> function, ChatType chatType, UUID uUID) {
-        if (!filteredText.getString().startsWith("/")) {
+    @Inject(method = "handleChat(Lnet/minecraft/server/network/TextFilter$FilteredText;)V", at = @At("HEAD"))
+    private void injectHandleChat(TextFilter.FilteredText filteredText, CallbackInfo ci) {
+        Component message = new TextComponent(filteredText.getRaw());
+
+        if (!message.getString().startsWith("/")) {
             if (FabricLoader.getInstance().isModLoaded("fabrictailor")) {
-                SafeCalls.tailerPlayerMessage(player, filteredText);
+                SafeCalls.tailerPlayerMessage(player, message);
             } else {
                 ServerEvents.getInstance().onServerChatEvent(
-                        filteredText,
+                        message,
                         player.getDisplayName(),
                         player.getUUID().toString()
                 );
             }
         }
-        instance.broadcastMessage(filteredText, function, chatType, uUID);
     }
 }
